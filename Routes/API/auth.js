6ba73a93ca95d48ecc1ecb890aa22cc8264e6802
Usama Bin Sadiq <usama.gormani8300@gmail.com>
2021-08-26 16:59:44 +0500

    Custom Auth Middleware JWT Verification

--- Routes/API/auth.js
+++ Routes/API/auth.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
-
+const auth = require("../../middleware/auth");
+const User = require("../../models/User")
 //@route    Get api/auth
 //@desc     Test Route
 //@acces    Public / Private
-router.get("/", (req, res) => res.send("Auth Route"));
+router.get("/", auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error");
+    }
+});
 
 module.exports = router;
